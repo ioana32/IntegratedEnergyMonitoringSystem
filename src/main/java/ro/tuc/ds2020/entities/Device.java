07bfbea288b649a9730d932b95e22cf237f5e9de
Ioana <i_virna@yahoo.com>
2022-10-18 22:23:29 +0300

    init

---
+++ src/main/java/ro/tuc/ds2020/entities/Device.java
@@ -0,0 +1,71 @@
+package ro.tuc.ds2020.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Device {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Type(type = "uuid-binary")
+    private UUID id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "maxEnergy", nullable = false)
+    private int maxEnergy;
+
+
+    public Device() {
+    }
+
+    public Device(String description, String address, int maxEnergy) {
+        this.description = description;
+        this.address = address;
+        this.maxEnergy = maxEnergy;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+
+    public void setMaxEnergy(int maxEnergy) {
+        this.maxEnergy = maxEnergy;
+    }
+}
---
+++ src/main/java/ro/tuc/ds2020/entities/User.java
@@ -0,0 +1,60 @@
+package ro.tuc.ds2020.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Type(type = "uuid-binary")
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "role", nullable = false)
+    private String address;
+
+    public User() {
+    }
+
+    public User(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
